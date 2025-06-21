bc403b8ce2cd2d18d5991fc6f71b0d0e28ce9f1c
theMaximmus <m.stesev@gmail.com>
2025-06-21 10:25:25 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Team Name: SF Pythons
+#
+# Team Members:
+#
+# 1. Maksym Stesev
+#
+# 2. Micole Chen
+#
+# 3. Samantha Chin
+
+def display_menu():
+    print("*" * 10)
+    print("Welcome to the College Food Court!")
+    print("Please choose from the following:")
+    print("1. De Anza Burger - $5.25")
+    print("2. Bacon Cheese - $5.75")
+    print("3. Mushroom Swiss - $5.95")
+    print("4. Western Burger - $5.95")
+    print("5. Don Cali Burger - $5.95")
+    print("6. Exit")
+    print("*" * 10)
